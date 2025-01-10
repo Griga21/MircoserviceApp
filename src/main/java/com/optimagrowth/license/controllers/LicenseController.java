2a1d4af519276db1c2dc87b98833fdce1b004e2b
Griga21 <griga.k.r21@gmail.com>
2025-01-10 09:16:22 +0300

    add new License class

---
+++ src/main/java/com/optimagrowth/license/controllers/LicenseController.java
@@ -0,0 +1,9 @@
+package com.optimagrowth.license.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/com.optimagrowth.license")
+public class LicenseController {
+}
---
+++ src/main/java/com/optimagrowth/license/models/License.java
@@ -0,0 +1,15 @@
+package com.optimagrowth.license.models;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class License {
+    private int id;
+    private String licenseId;
+    private String description;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
+}
